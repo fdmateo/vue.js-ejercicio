dbd95af545e9a744560c82ff8ff1b101fd4b6d58
John Doe <matmateo@hacienda.gov.do>
2025-06-11 15:50:09 -0400

    Mensaje describiendo los cambios

---
+++ js/09.js
@@ -0,0 +1,7 @@
+//Array o Arreglos 
+
+const tecnologia = ['vue','cala','hello', 'gala']
+
+// console.table (tecnologia[2])
+console.log (tecnologia)
+
---
+++ js/10.js
@@ -0,0 +1,14 @@
+//Array o Arreglos 
+
+const tecnologia = ['vue','cala','hello', 'gala']
+
+
+
+// console.table (tecnologia[2])
+
+
+//esto copia el arreglo que tiene
+const nuevoArreglo = [...tecnologia,'kkaka']
+console.log (tecnologia)
+console.log(nuevoArreglo)
+
