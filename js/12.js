9518b6b4ffcb8101e0323d0832bca66bed6875d0
John Doe <matmateo@hacienda.gov.do>
2025-06-12 15:43:12 -0400

    Mensaje describiendo los cambios

--- js/12.js
+++ js/12.js
@@ -4,7 +4,8 @@ const tecnologia = ['vue', 'cala', 'hello', 'gala']
 
 
 
-//forEach  
+//forEach  Valor de retorno: forEach siempre devuelve undefined. Esto significa que no crea un nuevo array a partir de los resultados de la función callback. 
+//Su objetivo es simplemente realizar una acción por cada elemento, como imprimirlo en la consola, modificar un valor externo, etc.
 
 const arrayForEach = tecnologia.forEach(function (tech) {
     return tech
@@ -13,12 +14,13 @@ const arrayForEach = tecnologia.forEach(function (tech) {
 
 
 
-//map
+//map  Propósito principal: Iterar sobre cada elemento de un array, 
+//ejecutar una función (callback) para cada uno, y crear un nuevo array con los resultados de cada llamada a la función.
 
 const arrayMap = tecnologia.map(function (tech) {
     return tech
 
 })
 
-//console.log(arrayForEach)
+console.log(arrayForEach)
 console.log(arrayMap) 
