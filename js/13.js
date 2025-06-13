eb72832e083822f689c439c8d67d9471dc9abb48
John Doe <matmateo@hacienda.gov.do>
2025-06-13 09:14:13 -0400

    Mensaje describiendo los cambios

---
+++ js/13.js
@@ -0,0 +1,15 @@
+//Function Declaraction, es una piesa de codigo que se puede reutilizar y que cumplar el proposito
+// una funcion no puede inicial con numero
+
+function sumar (numero1 = 8,numero2 = 5){
+
+    console.log(numero1+numero2)
+}
+
+
+// cuando declara una funcion tiene que mandarla a llamar despues y es reutilizable
+
+sumar(1,5)
+sumar(50,7899)
+sumar()
+
