0c503866f6a1ac78aaba03f962619ea779a155f6
John Doe <matmateo@hacienda.gov.do>
2025-06-13 09:38:31 -0400

    Mensaje describiendo los cambios

---
+++ js/14.js
@@ -0,0 +1,15 @@
+// Function Expression
+
+
+
+const sumar = function  (numero1 = 8,numero2 = 5){
+
+    console.log(numero1+numero2)
+}
+
+
+
+sumar(1,5)
+sumar(50,7899)
+sumar()
+
---
+++ js/15.js
@@ -0,0 +1,8 @@
+//Arrow functions y sus carateristica
+
+
+
+function sumar (numero1 = 8,numero2 = 5){
+
+    console.log(numero1+numero2)
+}
