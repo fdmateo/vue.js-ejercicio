61f3b6776ffc485fef1e75ac54a18def078874e8
John Doe <matmateo@hacienda.gov.do>
2025-06-16 11:20:12 -0400

    Mensaje describiendo los cambios

--- js/14.js
+++ js/14.js
@@ -1,13 +1,13 @@
 // Function Expression
 
 
-
-const sumar = function  (numero1 = 8,numero2 = 5){
+// puede utilizar  cuando este asi const sumar utilizar las funciones si tener que poner function utilizando =>
+const sumar =  (numero1 = 8,numero2 = 5)=>{
 
     console.log(numero1+numero2)
 }
 
-
+//const sumar = function  (numero1 = 8,numero2 = 5){
 
 sumar(1,5)
 sumar(50,7899)
--- js/15.js
+++ js/15.js
@@ -2,7 +2,58 @@
 
 
 
-function sumar (numero1 = 8,numero2 = 5){
+// function sumar(numero1 = 8, numero2 = 5) {
 
-    console.log(numero1+numero2)
+
+
+//     console.log(numero1 + numero2)
+// }
+
+
+const suma = function (numero1 = 20, numero2 = 30){
+
+    console.log (numero1 + numero2)
+
+    
+
 }
+
+suma();
+
+
+
+
+
+//forEach  Valor de retorno: forEach siempre devuelve undefined. Esto significa que no crea un nuevo array a partir de los resultados de la función callback. 
+//Su objetivo es simplemente realizar una acción por cada elemento, como imprimirlo en la consola, modificar un valor externo, etc.
+
+// const arrayForEach = tecnologia.forEach(function (tech) {
+//     return tech
+
+// })
+
+
+
+const tecnologia = ['vue', 'cala', 'hello', 'gala']
+
+
+const arrayForEach = tecnologia.forEach( (tech) => {
+  console.log(tecnologia)
+
+})
+
+
+
+
+//map  Propósito principal: Iterar sobre cada elemento de un array, 
+//ejecutar una función (callback) para cada uno, y crear un nuevo array con los resultados de cada llamada a la función.
+
+// const arrayMap = tecnologia.map(function (tech) {
+//     return tech
+
+// })
+
+
+// sumar(1,5)
+// sumar(50,7899)
+// sumar()
